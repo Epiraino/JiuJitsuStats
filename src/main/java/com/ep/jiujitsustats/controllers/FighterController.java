d2039fa94a905337d8ffdb2c30498db4d33107a5
Emilio Piraino <piraino91@gmail.com>
2022-12-21 10:08:13 -0800

    fighter controller added DynamoDB

--- src/main/java/com/ep/jiujitsustats/controllers/FighterController.java
+++ src/main/java/com/ep/jiujitsustats/controllers/FighterController.java
@@ -1,5 +1,7 @@
 package com.ep.jiujitsustats.controllers;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.ep.jiujitsustats.enums.Belt;
 import com.ep.jiujitsustats.models.Fighter;
 import com.ep.jiujitsustats.services.FighterService;
@@ -14,11 +16,16 @@ import java.util.List;
 @RequestMapping(path = "api/v1/fighters")
 public class FighterController {
     private final FighterService fighterService;
-
+    private final DynamoDBMapper dynamoDBMapper;
     @Autowired
-    public FighterController(FighterService fighterService) {
+    public FighterController(FighterService fighterService, DynamoDBMapper dynamoDBMapper) {
         this.fighterService = fighterService;
+        this.dynamoDBMapper = dynamoDBMapper;
     }
+
+
+
+
 
     @GetMapping
     public List<Fighter> getFighters(){
